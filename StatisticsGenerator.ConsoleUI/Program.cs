f215b41bb702d50819baf6efcc81e6c4752dfbb7
Frank J. Reashore <reashore@telus.net>
2016-04-17 13:13:38 -0700

    Added try/catch block

--- StatisticsGenerator.ConsoleUI/Program.cs
+++ StatisticsGenerator.ConsoleUI/Program.cs
@@ -10,15 +10,27 @@ namespace StatisticsGenerator.ConsoleUI
         public static void Main()
         {
             // for simplicity in this demo, just hard-code these file locations
-            const string basePath = "../../Data";
+            const string basePath = @"..\..\Data";
             string configurationFile = Path.Combine(basePath, "Configuration.txt");
             string inputDataFile = Path.Combine(basePath, "InputData.txt");
             string outputDataFile = Path.Combine(basePath, "OutputData.txt");
 
-            StatsGenerator statsGenerator = new StatsGenerator(configurationFile);
-            string statisticalResults = statsGenerator.GenerateStatistics(inputDataFile, outputDataFile);
+            try
+            {
+                StatsGenerator statsGenerator = new StatsGenerator(configurationFile);
+                string statisticalResults = statsGenerator.GenerateStatistics(inputDataFile, outputDataFile);
 
-            Console.WriteLine($"\n{statisticalResults}");
+                Console.WriteLine($"\nConfiguration file = {configurationFile}");
+                Console.WriteLine($"Input Data file    = {configurationFile}");
+                Console.WriteLine($"Output file        = {configurationFile}");
+                Console.WriteLine($"\n{statisticalResults}");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+                
+            }
+
             Console.WriteLine("Press any key to exit");
             Console.ReadKey();
         }
