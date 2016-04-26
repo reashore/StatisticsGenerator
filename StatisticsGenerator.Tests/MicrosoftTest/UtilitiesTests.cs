1ec35bfd7008ed062fdab1683cdb74f106b2e029
Frank J. Reashore <reashore@telus.net>
2016-04-25 18:48:01 -0700

    Code cleanup

--- StatisticsGenerator.Tests/MicrosoftTest/UtilitiesTests.cs
+++ StatisticsGenerator.Tests/MicrosoftTest/UtilitiesTests.cs
@@ -1,11 +1,10 @@
 ﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using StatisticsGenerator.Domain;
 
-// Required for TimePlinqConcurrencyTest which is commented out
-// ReSharper disable once RedundantUsingDirective
 using Stopwatch = System.Diagnostics.Stopwatch;
 
 namespace StatisticsGenerator.Tests.MicrosoftTest
@@ -41,28 +40,45 @@ namespace StatisticsGenerator.Tests.MicrosoftTest
             Assert.AreEqual(expectedStandardDeviation, actualStandardDeviation);
         }
 
-        //[TestMethod]
-        //public void TimePlinqConcurrencyTest()
-        //{
-        //    // Arrange
-        //    IEnumerable<double> sequence = Enumerable.Range(1, 10000000).Select(n => (double)n);
+        [TestMethod]
+        public void ConcurrentAndNonconcurrentAggregationsAreEqualTest()
+        {
+            // Arrange
+            IEnumerable<double> sequence = Enumerable.Range(1, 10000000).Select(n => (double)n);
+            const double delta = .0001;
 
-        //    // Act
-        //    // ReSharper disable PossibleMultipleEnumeration
-        //    // ReSharper disable UnusedVariable
-        //    Stopwatch stopwatch = Stopwatch.StartNew();
-        //    double sumWithoutConcurrency = sequence.Sum();
-        //    TimeSpan elapsedWithoutConcurrency = stopwatch.Elapsed;
+            // Act
+            // ReSharper disable PossibleMultipleEnumeration
+            double sumWithoutConcurrency = sequence.Sum();
+            double sumWithConcurrency = sequence.AsParallel().Sum();
+            // ReSharper restore PossibleMultipleEnumeration
 
-        //    stopwatch = Stopwatch.StartNew();
-        //    double sumWithConcurrency = sequence.AsParallel().Sum();
-        //    TimeSpan elapsedWithConcurrency = stopwatch.Elapsed;
-        //    // ReSharper restore UnusedVariable
-        //    // ReSharper restore PossibleMultipleEnumeration
+            // Assert
+            Assert.AreEqual(sumWithoutConcurrency, sumWithConcurrency, delta);
+        }
 
-        //    // Assert
-        //    bool concurrentCalculationIsFaster = (elapsedWithConcurrency < elapsedWithoutConcurrency);
-        //    Assert.IsTrue(concurrentCalculationIsFaster);
-        //}
+        [TestMethod]
+        public void ConcurrentAggregationIsSlowerThanNonconcurrentAggregationTest()
+        {
+            // Arrange
+            IEnumerable<double> sequence = Enumerable.Range(1, 10000000).Select(n => (double)n);
+
+            // Act
+            // ReSharper disable PossibleMultipleEnumeration
+            // ReSharper disable UnusedVariable
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            double sumWithoutConcurrency = sequence.Sum();
+            TimeSpan elapsedWithoutConcurrency = stopwatch.Elapsed;
+
+            stopwatch = Stopwatch.StartNew();
+            double sumWithConcurrency = sequence.AsParallel().Sum();
+            TimeSpan elapsedWithConcurrency = stopwatch.Elapsed;
+            // ReSharper restore UnusedVariable
+            // ReSharper restore PossibleMultipleEnumeration
+
+            // Assert
+            bool concurrentCalculationIsFaster = (elapsedWithConcurrency >= elapsedWithoutConcurrency);
+            Assert.IsTrue(concurrentCalculationIsFaster);
+        }
     }
 }
--- StatisticsGenerator.Tests/XUnit/XUnitUnitTests.cs
+++ StatisticsGenerator.Tests/XUnit/XUnitUnitTests.cs
@@ -1,23 +1,49 @@
 ﻿
+using System;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace StatisticsGenerator.Tests.XUnit
 {
-    public class XUnitTests
+    public sealed class XUnitTests : IDisposable
     {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public XUnitTests(ITestOutputHelper testOutputHelper)
+        {
+            // constructor is run before each test
+            _testOutputHelper = testOutputHelper;
+        }
+
         [Fact]
+        [Trait("Category", "Basic")]
         public void XUnitTemplateTest()
         {
             //Arrange
+            _testOutputHelper.WriteLine("Arrange");
             const double expectedResult = 3.00001;
 
             // Act
+            _testOutputHelper.WriteLine("Act");
             const double actualResult = 3.00000;
 
             // Assert
+            _testOutputHelper.WriteLine("Assert");
             Assert.Equal(true, true);
             Assert.Equal(expectedResult, actualResult, 3);
             Assert.NotEqual(1, 2);
+            Assert.Equal("FOO", "Foo", ignoreCase: true);
+            Assert.Contains("foo", "foobar");
+            Assert.StartsWith("foo", "foobar");
+            Assert.False(false);
+            //Assert.Null(null);
+        }
+
+        [Fact]
+        [Trait("Category", "Exception")]
+        public void CheckThatExceptionIsThrownTest()
+        {
+            Assert.Throws<Exception>(() => Calculator.ThrowException());
         }
 
         [Theory]
@@ -35,6 +61,11 @@ namespace StatisticsGenerator.Tests.XUnit
             // Assert
             Assert.Equal(expectedSum, actualSum);
         }
+
+        public void Dispose()
+        {
+            // cleanup after each test
+        }
     }
 
     // ReSharper disable once ClassNeverInstantiated.Global
@@ -43,6 +74,11 @@ namespace StatisticsGenerator.Tests.XUnit
         public static int Add(int x, int y)
         {
             return x + y;
+        }
+
+        public static void ThrowException()
+        {
+            throw new Exception("This is an exception");
         }
     }
 }
